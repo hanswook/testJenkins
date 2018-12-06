9663beb1243b9151cdce54169e4bac8514df2439
hanxu <hxxx1992@163.com>
2018-12-06 17:33:57 +0800

    add  haha

---
+++ app/src/main/java/com/hans/testjenkins/JenkinsHansUtils.java
@@ -0,0 +1,11 @@
+package com.hans.testjenkins;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class JenkinsHansUtils {
+
+    public void showHans(Context context) {
+        Toast.makeText(context, "hahaha  hans", Toast.LENGTH_SHORT).show();
+    }
+}

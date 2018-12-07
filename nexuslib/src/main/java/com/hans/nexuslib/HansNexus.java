655072241383cc6152c0b3c77d9b18bbeda7d570
hanxu <hxxx1992@163.com>
2018-12-07 11:28:48 +0800

    add cc

--- nexuslib/src/main/java/com/hans/nexuslib/HansNexus.java
+++ nexuslib/src/main/java/com/hans/nexuslib/HansNexus.java
@@ -3,7 +3,8 @@ package com.hans.nexuslib;
 public class HansNexus {
 
     public static String hahaha() {
-        String a="a";
+        String a = "a";
+        String c = "c";
         return "我是nexus测试第一包";
     }
 }

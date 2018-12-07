756b5d6da76bee376aafd232dd3df66678cbb3ab
hanxu <hxxx1992@163.com>
2018-12-07 11:27:02 +0800

    test b

--- nexuslib/src/main/java/com/hans/nexuslib/HansNexus.java
+++ nexuslib/src/main/java/com/hans/nexuslib/HansNexus.java
@@ -3,7 +3,8 @@ package com.hans.nexuslib;
 public class HansNexus {
 
     public static String hahaha() {
-        String a="a";
+        String a = "a";
+        String b = "b";
         return "我是nexus测试第一包";
     }
 }

2b880faabb73691945a317a68c96abf0d0143747
hanxu <hxxx1992@163.com>
2018-12-07 11:42:31 +0800

    aaxxxx

---
+++ nexuslib/src/main/java/com/hans/nexuslib/HansNexus.java
@@ -0,0 +1,11 @@
+package com.hans.nexuslib;
+
+public class HansNexus {
+
+    public static String hahaha() {
+        String a = "a";
+        String c = "c";
+        String b = "b";
+        return "我是nexus测试第一包";
+    }
+}
